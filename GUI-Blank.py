02a81ee55393afea687d14c0d212c5a258f30af1
trad2k <trad2k@outlook.com>
2022-06-15 18:15:29 +0300

    GUI-GraphApp started

---
+++ GUI-Blank.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+
+layout = [[]]
+
+window = sg.Window('Blank App', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
