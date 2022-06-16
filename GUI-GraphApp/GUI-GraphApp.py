0b89d660a95b903d1a2c40bfa0606f640c94b2ac
trad2k <trad2k@outlook.com>
2022-06-16 18:49:04 +0300

    GUI-GraphApp basic layout

--- GUI-GraphApp/GUI-GraphApp.py
+++ GUI-GraphApp/GUI-GraphApp.py
@@ -1,6 +1,16 @@
 import PySimpleGUI as sg
 
-layout = [[]]
+sg.theme('DarkTeaL6')
+
+table_content = []
+layout = [
+    [sg.Table(headings=['Observation', 'Result'],
+              values=table_content,
+              expand_x=True,
+              hide_vertical_scroll=True,
+              key='TABLE-')],
+    [sg.Input(key='-INPUT-', expand_x=True), sg.Button('Submit')]
+]
 
 window = sg.Window('GUI-GraphApp', layout)
 
@@ -8,5 +18,10 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == 'Submit':
+        new_value = values['-INPUT-']
+        if new_value.isnumeric():
+            window['-TABLE-'].update([[1, 10],[2, 12], [3,0]])
 
 window.close()
