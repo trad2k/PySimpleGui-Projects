5233a66be601533061740f681d45138145a84938
trad2k <trad2k@outlook.com>
2022-06-19 23:00:34 +0300

    GUI-Image Added image manipulation function

--- GUI-Image Editor/GUI-Image Editor.py
+++ GUI-Image Editor/GUI-Image Editor.py
@@ -1,12 +1,63 @@
 import PySimpleGUI as sg
+from PIL import Image, ImageFilter, ImageOps
+from io import BytesIO
+from pathlib import Path
 
-layout = [[]]
 
+def update_image(original, blur, contrast, emboss, contour, flipx, flipy):
+    image = original.filter(ImageFilter.GaussianBlur(blur))
+    image = image.filter(ImageFilter.UnsharpMask(contrast))
+
+    if emboss:
+        image = image.filter(ImageFilter.EMBOSS())
+    if contour:
+        image = image.filter(ImageFilter.CONTOUR())
+
+    if flipx:
+        image = ImageOps.mirror(image)
+    if flipy:
+        image = ImageOps.flip(image)
+
+    bio = BytesIO()
+    image.save(bio, format='PNG')
+    window['-IMAGE-'].update(data=bio.getvalue())
+
+
+image_path = 'test.png'
+
+control_col = sg.Column([
+    [sg.Frame('Blur', layout=[[sg.Slider(range=(0, 10), orientation='h', key='-BLUR-')]])],
+    [sg.Frame('Contrast', layout=[[sg.Slider(range=(0, 10), orientation='h', key='-CONTRAST-')]])],
+    [sg.Checkbox('Emboss', key='-EMBOSS-'), sg.Checkbox('Contour', key='-CONTOUR-')],
+    [sg.Checkbox('Flip x', key='-FLIPX-'), sg.Checkbox('Flip Y', key='-FLIPY-')],
+    [sg.Button('Save Image', key='-SAVE-')],
+])
+image_col = sg.Column([[sg.Image(image_path, key='-IMAGE-')]])
+layout = [[control_col, image_col]]
+
+original = Image.open(image_path)
 window = sg.Window('GUI-Image Editor', layout)
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)
     if event == sg.WIN_CLOSED:
         break
+
+    if event == '-SAVE-':
+        file_path = sg.popup_get_file('Save as', no_window=True, save_as=True)
+        if file_path[-4:] != '.png':
+            file_path += '.png'
+        file = Path(file_path)
+        image.save(file, format='PNG')
+
+    update_image(
+        original,
+        values['-BLUR-'],
+        values['-CONTRAST-'],
+        values['-EMBOSS-'],
+        values['-CONTOUR-'],
+        values['-FLIPX-'],
+        values['-FLIPY-']
+    )
 
 window.close()
